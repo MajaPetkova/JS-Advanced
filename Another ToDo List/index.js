35e028198c113a8621d45c9d35011f358d6d713d
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-09 22:49:06 +0100

    Update index.js

--- Another ToDo List/index.js
+++ Another ToDo List/index.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", () => {
-  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
   const nameInput = document.getElementById("name");
-  //   const todoList = document.getElementById("todo-list");
   const newTodoForm = document.querySelector("#new-todo-form");
+
   const username = localStorage.getItem("username") || [];
   nameInput.value = username;
 
@@ -12,14 +12,62 @@ window.addEventListener("load", () => {
   });
 
   newTodoForm.addEventListener("submit", onSubmit);
+  function onSubmit(ev) {
+    ev.preventDefault();
+    const todo = {
+      content: ev.target.elements.content.value,
+      category: ev.target.elements.category.value,
+      done: false,
+      createdAt: new Date(),
+    };
+    //   const formData = new FormData(ev.target);
+    //   const content = formData.get("content");
+    //   const category = formData.get("category");
+    //   const done = false;
+    //   const createdAt = new Date();
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    ev.target.reset();
+    // console.log(todos);
+    displayTodos();
+  }
 });
 
-function onSubmit(ev) {
-  ev.preventDefault();
-  const formData = new FormData(ev.target);
-  const content = formData.get("content");
-  const category = formData.get("category");
-  const done = false;
-  const createdAt = new Date();
-  console.log(createdAt);
+function displayTodos() {
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item");
+
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = todo.done;
+
+    const span = document.createElement("span");
+    span.className = "bubble";
+
+    if (todo.category == "personal") {
+      span.className = "personal";
+    } else {
+      span.className = "business";
+    }
+
+    const content = document.createElement("div");
+    content.className = "content";
+
+    const actions = document.createElement("div");
+    actions.className = "actions";
+
+    const edit = document.createElement("button");
+    edit.className = "edit";
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete";
+
+    content.innerHTML = '<input type="text" value="${todo.content}" readonly>';
+    edit.innerHTML = "Edit";
+    deleteBtn.innerHTML = "Delete";
+  });
 }
