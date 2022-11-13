3ac38255c7eb8450e9f018514be7188d68172004
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-13 11:16:49 +0100

    Update app.js

--- Exam Prep2/21.Dish Manager/app.js
+++ Exam Prep2/21.Dish Manager/app.js
@@ -1,5 +1,34 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ....
+  const firstNameEl = document.getElementById("first-name");
+  const lastNameEl = document.getElementById("last-name");
+  const ageEl = document.getElementById("age");
+  const genderEl = document.getElementById("genderSelect");
+  const descriptionEl = document.getElementById("task");
+  const submitBtn = document.getElementById("form-btn");
+
+  submitBtn.addEventListener("click", onSubmit);
+
+  function onSubmit() {
+    const firstName = firstNameEl.value.trim();
+    const lastName = lastNameEl.value.trim();
+    const age = Number(ageEl.value).trim();
+    const gender = genderEl.value;
+    const description = descriptionEl.value.trim();
+    if(firstName=='' || lastName== '' || age=='' || Number.isNaN(age) || description==''){
+      return;
+    }
+    console.log('valid')
+  }
+
+  function e(type, attr, ...content){
+    const element= document.createElement(type);
+    for(let prop in attr){
+      element[prop]= attr[prop];
+    }
+    for(let item of content){
+      element.appendChild(item)
+    }
+  }
 }
