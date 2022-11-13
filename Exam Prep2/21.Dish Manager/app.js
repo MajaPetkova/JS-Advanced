885b7ba0f18660dd3f0d326da287fc51b7952fe4
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-13 21:52:09 +0100

    Update app.js

--- Exam Prep2/21.Dish Manager/app.js
+++ Exam Prep2/21.Dish Manager/app.js
@@ -7,9 +7,11 @@ function solve() {
   const genderEl = document.getElementById("genderSelect");
   const descriptionEl = document.getElementById("task");
   const submitBtn = document.getElementById("form-btn");
-  const inProgress= document.getElementById('in-progress');
+  const inProgress = document.getElementById("in-progress");
+  const dishesCount = document.getElementById("progress-count");
+  let dishesCountNumber = Number(dishesCount.textContent);
   submitBtn.addEventListener("click", onSubmit);
-
+  
   function onSubmit() {
     const firstName = firstNameEl.value.trim();
     const lastName = lastNameEl.value.trim();
@@ -25,26 +27,26 @@ function solve() {
     ) {
       return;
     }
-    const manager= document.createElement('li');
-    manager.classList= 'each-line';
-    const article= document.createElement('article');
+    const manager = document.createElement("li");
+    manager.classList = "each-line";
+    const article = document.createElement("article");
 
-    const title= document.createElement('h4');
-    title.textContent= firstName + ' ' + lastName;
+    const title = document.createElement("h4");
+    title.textContent = firstName + " " + lastName;
 
-    const para= document.createElement('p');
-    para.textContent= gender + ', ' + age;
+    const para = document.createElement("p");
+    para.textContent = gender + ", " + age;
 
-    const desc= document.createElement('p');
-    desc.textContent= description;
+    const desc = document.createElement("p");
+    desc.textContent = description;
 
-    const editBtn= document.createElement('button');
-    editBtn.className='edit-btn';
-    editBtn.textContent= 'Edit';
-    
-    const completeBtn= document.createElement('button');
-    completeBtn.className='complete-btn';
-    completeBtn.textContent= 'Mark as complete';
+    const editBtn = document.createElement("button");
+    editBtn.className = "edit-btn";
+    editBtn.textContent = "Edit";
+
+    const completeBtn = document.createElement("button");
+    completeBtn.className = "complete-btn";
+    completeBtn.textContent = "Mark as complete";
 
     manager.appendChild(article);
     article.appendChild(title);
@@ -52,18 +54,18 @@ function solve() {
     article.appendChild(desc);
     manager.appendChild(editBtn);
     manager.appendChild(completeBtn);
-    
+
     inProgress.appendChild(manager);
-   
+
     firstNameEl.value = "";
     lastNameEl.value = "";
     ageEl.value = "";
     descriptionEl.value = "";
   
     // console.log(firstName, lastName, age, description, gender);
-
+    dishesCountNumber++;
+     dishesCount.textContent = dishesCountNumber;
   }
-
   // function e(type, attr, ...content) {
   //   const element = document.createElement(type);
   //   for (let prop in attr) {
