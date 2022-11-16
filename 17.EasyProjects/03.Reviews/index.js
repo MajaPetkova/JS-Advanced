0e679cde2f719a369f611ff0afe23140c0ee206c
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-16 16:26:50 +0100

    Updated solution

--- 17.EasyProjects/03.Reviews/index.js
+++ 17.EasyProjects/03.Reviews/index.js
@@ -10,7 +10,7 @@ const reviews = [
     id: 2,
     name: "Maria Jonson",
     job: "Web Designer",
-    img: "https://www.iodocs.com/wp-content/uploads/2019/01/female-web-developers.jpg",
+    img: "https://pbs.twimg.com/profile_images/1525509316882616320/AfBZNRUV_400x400.jpg",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo reiciendis omnis, eaque voluptatum laudantium voluptates blanditiis veritatis eum architecto earum;",
   },
   {
@@ -18,12 +18,14 @@ const reviews = [
     name: "Maria Jonson",
     job: "Manager",
     img: "https://cdn.utorontobootcamp.com/wp-content/uploads/sites/97/2019/06/BlogImage_WebDeveloper5.jpg",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo reiciendis omnis, eaque voluptatum laudantium voluptates blanditiis veritatis eum architecto earum;",
   },
   {
     id: 4,
     name: "Alex Montoya",
     job: "Sales Manager",
     img: "https://webuniversals.co.in/wp-content/uploads/2016/02/mantrans02c.jpg",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo reiciendis omnis, eaque voluptatum laudantium voluptates blanditiis veritatis eum architecto earum;",
   },
 ];
 
@@ -32,20 +34,32 @@ const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-const randomBtn = document.querySelector("random-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.getElementById("random");
 
 //set starting item
-let currentItem = 3;
+let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", onLoad);
 
 function onLoad() {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  randomBtn.textContent = item.info;
+showPerson(currentItem)
+}
+
+//show person based on item
+
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+//show next person
+nextBtn.addEventListener('click', getNextPerson);
+function getNextPerson(){
+   currentItem++; 
+   showPerson(currentItem)
 }
