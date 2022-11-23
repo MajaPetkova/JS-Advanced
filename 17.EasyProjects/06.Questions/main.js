9bb2bbcfa4b80476f30e7d32459c4c7b34809483
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-23 17:08:14 +0100

    Update main.js

--- 17.EasyProjects/06.Questions/main.js
+++ 17.EasyProjects/06.Questions/main.js
@@ -1,11 +1,30 @@
+// Traversing the DOM
 
-// Traversing the DOM 
+// const buttons = document.querySelectorAll(".question-btn");
 
-const buttons = document.querySelectorAll(".question-btn");
+// buttons.forEach((x) => x.addEventListener("click", onToggle));
 
-buttons.forEach((x) => x.addEventListener("click", onToggle));
+// function onToggle(e) {
+//   const question=e.currentTarget.parentElement.parentElement;
+//   question.classList.toggle("show-text")
+// }
 
-function onToggle(e) {
-  const question=e.currentTarget.parentElement.parentElement;
-  question.classList.toggle("show-text")
-}
+// using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((x) => {
+  // console.log(x)
+  const btn = x.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", onToggle);
+  function onToggle() {
+    questions.forEach((y) => {
+      if (y !== x) {
+        y.classList.remove('show-text')
+      }
+    });
+    x.classList.toggle("show-text");
+  }
+});
