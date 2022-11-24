264d8e68a1faa689f1a708f4a0b6974bbd40f1c6
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-24 12:29:29 +0100

    Update solution for 07.Menu

--- 17.EasyProjects/07.Menu/index.js
+++ 17.EasyProjects/07.Menu/index.js
@@ -42,25 +42,37 @@ const menu = [
     title: "Oreo shake Chocolate-Vanilla",
     price: 6.99,
     category: "shakes",
-    img:"https://images.unsplash.com/photo-1638176066666-ffb2f013c7dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2hha2VzfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=400&q=60",
+    img: "https://images.unsplash.com/photo-1638176066666-ffb2f013c7dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2hha2VzfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=400&q=60",
     description:
       "Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack.",
   },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtn=document.querySelectorAll('.filter-btn');
+const filterBtn = document.querySelectorAll(".filter-btn");
+const btnContainer= document.querySelector('.btn-container')
 
 window.addEventListener("DOMContentLoaded", onLoad);
 
 // load Items
 function onLoad() {
- displayMenuItems(menu)
+  displayMenuItems(menu);
+
+  const categories = menu.reduce((values, item) => {
+    if(!values.includes(item.category)){
+        values.push(item.category)
+    }
+    return values
+  }, ["all"]);
+  const categoryBtn=categories.map(x =>{
+    return `<button class="filter-btn" type="button" data-id="${x}">${x}</button>`
+  })
+  btnContainer.innerHTML = categoryBtn.join('')
 }
 
-function displayMenuItems(menuItems){
-    let displayMenu = menuItems.map((x) => {
-        return `<article class="menu-item">
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map((x) => {
+    return `<article class="menu-item">
         <img src="${x.img}">
         <div class="item-info">
             <header>
@@ -70,35 +82,33 @@ function displayMenuItems(menuItems){
             <p class="item-text">${x.description}</p>
         </div>
     </article>`;
-      });
-      displayMenu = displayMenu.join("");
-      sectionCenter.innerHTML = displayMenu;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
 }
 
 // filter Items
-filterBtn.forEach(x=>{
-    x.addEventListener('click', onFilter)
-})
+filterBtn.forEach((x) => {
+  x.addEventListener("click", onFilter);
+});
 
-function onFilter(e){
-  const category= e.currentTarget.dataset.id;
+function onFilter(e) {
+  const category = e.currentTarget.dataset.id;
 
-  const menuCategory = menu.filter(function(menuItems){
-   if(menuItems.category == category){
-       return menuItems
-   }
+  const menuCategory = menu.filter(function (menuItems) {
+    if (menuItems.category == category) {
+      return menuItems;
+    }
   });
-  if(category== 'all'){
-    displayMenuItems(menu)
-  }else if(category =='breakfast'){
-    displayMenuItems(menuCategory)
-  }else if(category =='lunch'){
-    displayMenuItems(menuCategory)
-  }else if(category =='shakes'){
-    displayMenuItems(menuCategory)
-  }else if(category =='dinner'){
-    displayMenuItems(menuCategory)
+  if (category == "all") {
+    displayMenuItems(menu);
+  } else if (category == "breakfast") {
+    displayMenuItems(menuCategory);
+  } else if (category == "lunch") {
+    displayMenuItems(menuCategory);
+  } else if (category == "shakes") {
+    displayMenuItems(menuCategory);
+  } else if (category == "dinner") {
+    displayMenuItems(menuCategory);
   }
-    
-
 }
