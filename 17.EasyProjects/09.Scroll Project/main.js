9590a30dd616b0007f0d12097b3d872655e3c1de
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-26 11:15:27 +0100

    Update solution for 09.Scroll Project

--- 17.EasyProjects/09.Scroll Project/main.js
+++ 17.EasyProjects/09.Scroll Project/main.js
@@ -18,19 +18,52 @@ function onToggle() {
   }
 }
 
-
-const navbar=document.getElementById('nav');
-const topLink=document.querySelector('.top-link');
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 
 window.addEventListener("scroll", onScroll);
 
 function onScroll() {
-  const scrollHeight= window.pageYOffset;
-  const navHeight= navbar.getBoundingClientRect().height;
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
 
-  if(scrollHeight > navHeight){
-    navbar.classList.add('fixed-nav')
-  }else{
-    navbar.classList.remove('fixed-nav')
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
   }
 }
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    let position = element.offsetTop - navHeight;
+
+    const fixedNav = navbar.classList.contains("fixed-nav");
+
+    if (!fixedNav) {
+      position = position;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    // console.log(position)
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
