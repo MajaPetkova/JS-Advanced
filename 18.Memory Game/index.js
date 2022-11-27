2836e00c49f46495d987183dc88213f8b4a1ac59
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-27 23:02:53 +0100

    Update index.js

--- 18.Memory Game/index.js
+++ 18.Memory Game/index.js
@@ -1,11 +1,14 @@
 const cards = document.querySelectorAll(".memory-card");
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard;
 let secondCard;
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
 function flipCard(e) {
+  if (lockBoard) return;
+  if(e.currentTarget== firstCard) return;
   e.currentTarget.classList.add("flip");
 
   if (!hasFlippedCard) {
@@ -17,16 +20,33 @@ function flipCard(e) {
     //second click
     hasFlippedCard = false;
     secondCard = e.currentTarget;
-
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-    }else{
-        setTimeout(()=>{
-
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip')
-        }, 1500)
-    }
+    checkForMatch();
   }
 }
+
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    disableCards();
+  } else {
+    unflipCards();
+  }
+}
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+  }, 1200);
+}
+
+(function shuffle(){
+  cards.forEach(card =>{
+    let randomPos=Math.floor(Math.random() * 12)
+    card.style.order = randomPos;
+  }) 
+}) ()
