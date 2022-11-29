c07c8220a20181379caaf828dae94904e6598386
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-29 23:12:25 +0100

    Update app.js

--- Exam Prep2/23.Forum Posts/app.js
+++ Exam Prep2/23.Forum Posts/app.js
@@ -1,19 +1,29 @@
 window.addEventListener("load", solve);
 
 function solve() {
-const titleEl= document.getElementById('post-title');
-const categoryEl= document.getElementById('post-category');
-const textAreaEl=document.getElementById('post-content');
-const publishBtn= document.getElementById("publish-btn");
-const postReviewList= document.getElementById('review-list');
+  const titleEl = document.getElementById("post-title");
+  const categoryEl = document.getElementById("post-category");
+  const textAreaEl = document.getElementById("post-content");
+  const publishBtn = document.getElementById("publish-btn");
+  const postReviewList = document.getElementById("review-list");
 
+  publishBtn.addEventListener("click", onPublish);
+  function onPublish(e) {
+    e.preventDefault();
+    if (
+      titleEl.value == "" &&
+      categoryEl.value == "" &&
+      textAreaEl.value == ""
+    ) {
+      return;
+    }
+    const post = document.createElement("li");
+    post.className = "rpost";
 
-publishBtn.addEventListener('click', onPublish)
-function onPublish(e){
-  e.preventDefault()
-if(titleEl.value == '' && categoryEl.value == '' && textAreaEl.value ==''){
-  return
-}
-  // console.log(titleEl.value, categoryEl.value, textAreaEl.value)
-}
+    const article = document.createElement("article");
+    post.appendChild(article);
+    post.appendChild(postReviewList)
+
+    // console.log(titleEl.value, categoryEl.value, textAreaEl.value);
+  }
 }
