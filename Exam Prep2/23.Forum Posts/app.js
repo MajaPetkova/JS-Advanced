c871cb12ca529da8092a83ab96cadafe8d80f5ae
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2022-11-29 22:57:16 +0100

    Update app.js for 23.Forum Post

--- Exam Prep2/23.Forum Posts/app.js
+++ Exam Prep2/23.Forum Posts/app.js
@@ -1,5 +1,19 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ...
+const titleEl= document.getElementById('post-title');
+const categoryEl= document.getElementById('post-category');
+const textAreaEl=document.getElementById('post-content');
+const publishBtn= document.getElementById("publish-btn");
+const postReviewList= document.getElementById('review-list');
+
+
+publishBtn.addEventListener('click', onPublish)
+function onPublish(e){
+  e.preventDefault()
+if(titleEl.value == '' && categoryEl.value == '' && textAreaEl.value ==''){
+  return
+}
+  // console.log(titleEl.value, categoryEl.value, textAreaEl.value)
+}
 }
