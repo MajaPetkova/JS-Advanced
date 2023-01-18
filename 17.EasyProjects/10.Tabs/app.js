251b5aa8bf37beb95747de6c36685636fe6d2e4f
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-18 23:10:52 +0100

    Added app.js

--- 17.EasyProjects/10.Tabs/app.js
+++ 17.EasyProjects/10.Tabs/app.js
@@ -1,5 +1,17 @@
 const buttons = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
-const element= document.querySelector(".articleContent")
-console.log(element)
+const about = document.querySelector(".container");
 
+about.addEventListener("click", onClick);
+
+function onClick(e) {
+  const id = e.target.dataset.id;
+
+  if (id) {
+    buttons.forEach((x) => x.classList.remove("active"));
+    e.target.classList.add("active");
+  }
+  articles.forEach((x)=> x.classList.remove("active"));
+  const element= document.getElementById(id); 
+  element.classList.add("active")
+}
