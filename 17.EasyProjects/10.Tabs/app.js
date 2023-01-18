034d4172ff3de1e929428b858fcaaa6fa75b249e
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-18 14:13:41 +0100

    Update html and css

---
+++ 17.EasyProjects/10.Tabs/app.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+const element= document.querySelector(".articleContent")
+console.log(element)
+
