8b5af326f37c0a71e89e5f98c83d8196ee693567
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-19 10:48:51 +0100

    Getting days, hours, minutes and seconds

--- 17.EasyProjects/11.Countdown/app.js
+++ 17.EasyProjects/11.Countdown/app.js
@@ -19,25 +19,49 @@ const weekdays = [
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
+  "Saturday",
 ];
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 // const currentDate= new Date();
 const futureDate = new Date(2023, 07, 27, 8, 30, 00);
-const year= futureDate.getFullYear();
-const hours= futureDate.getHours();
-const minutes= futureDate.getMinutes();
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
 
-const day= futureDate.getDate();
+const day = futureDate.getDate();
 
-let month= futureDate.getMonth();
-month= months[month];
+let month = futureDate.getMonth();
+month = months[month];
 
 let dayOfWeek = futureDate.getDay();
 dayOfWeek = weekdays[dayOfWeek];
 
-giveaway.textContent= `Giveaway ends on ${dayOfWeek}, ${day} ${month} ${year}, ${hours}:${minutes}am`
+giveaway.textContent = `Giveaway ends on ${dayOfWeek}, ${day} ${month} ${year}, ${hours}:${minutes}am`;
 
-console.log(dayOfWeek)
+//future time in milliseconds
+const futureTime = futureDate.getTime();
+// console.log(futureTime)
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureDate - today;
+  console.log(t);
+  // 1s = 1000ms
+  // 1min = 60s
+  // 1h = 60min
+  // 1day = 24hours
+
+  //values in ms:
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+  // const oneSec = 1000;
+
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMin);
+  let seconds = Math.floor((t % oneMin) / 1000);
+}
+getRemainingTime();
